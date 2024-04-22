e3d8b37491be9d73acca6654a80704ad3ba7be52
DB <alies22222@gmail.com>
2024-04-22 14:43:18 +0200

    added prisma files for the model and db changed the MealTransfer and Home to async

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import prisma from "../../lib/prisma";
 
-const test = () => {
+const test = async () => {
+
+  const da = await prisma.entry.findMany();
   return (
-    <div>test</div>
+    <div>
+      {da.map(da2 => <div key={da2.id}>
+        <h1>{da2.name}</h1>
+      </div>)}
+    </div>
   )
 }
+
+
 
 export default test

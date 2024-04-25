2f01eb5328a52c2c317ff99f11e68bf3a4d8e2a6
DB <alies22222@gmail.com>
2024-04-25 12:25:51 +0200

    post working

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Header: React.FC = () => {
+    return(<div></div>)
+};
+
+export default Header;
--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -7,8 +7,8 @@ import prisma from '../../../lib/prisma';
 export default async function handle(req, res) {
     const { name, recipe, ingredients, date } = req.body;
 
-    const session = await getSession({ req });
-    const result = await prisma.entry.create({
+    //const session = await getSession({ req });
+    const result = await prisma.post.create({
         data: {
             name: name,
             recipe: recipe,

4922dca4deb084ad5769d3103cfac78683536488
DB <alies22222@gmail.com>
2024-04-25 13:25:36 +0200

    ignore commit

--- app/upcomingd/page.tsx
+++ app/upcomingd/page.tsx
@@ -11,6 +11,10 @@ const Upcoming = async () => {
   const mealName2 = MealName("2");
   const mealIngredients2 = MealIngredients("2");
 
+  const mealName3 = MealName("3");
+  const mealIngredients3 = MealIngredients("3");
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-18">
       <div className="flex gap-4">
@@ -29,6 +33,15 @@ const Upcoming = async () => {
             <h1>{mealName2}</h1>
             <h3 className="pt-2 pr-2">Ingredients</h3>
             <div className="p-4">{mealIngredients2}</div>
+          </div>
+        </div>
+
+        {/* Today +3 */}
+        <div className="card-home flex justify-left items-left overflow-auto">
+          <div>
+            <h1>{mealName3}</h1>
+            <h3 className="pt-2 pr-2">Ingredients</h3>
+            <div className="p-4">{mealIngredients3}</div>
           </div>
         </div>
       </div>

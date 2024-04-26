0d902d1b32f55f89c3df4c7f99967938b4a608a3
Dejan <alies22222@gmail.com>
2024-04-26 17:00:29 +0200

    Home page, Navbar and upcoming page updated for phone use

--- app/components/MealTransfer/MealTransfer.tsx
+++ app/components/MealTransfer/MealTransfer.tsx
@@ -1,91 +1,117 @@
 import dayjs from "dayjs";
 import prisma from "../../../lib/prisma";
 
-
 const MealTransfer = async () => {
+  const meals = await prisma.post.findMany();
 
-    const meals = await prisma.post.findMany();
+  ////////////////////// Time Comparison
 
-    ////////////////////// Time Comparison
+  const datePrep = (date) => {
+    const stringDate = String(date);
+    const formatedCurrentDate = dayjs(stringDate)
+      .format("YYYY MM DD")
+      .replaceAll(" ", "-");
 
-    const datePrep = (date, num) => {
+    return formatedCurrentDate;
+  };
 
-        const lastCharacter = date.substring(0, 10).slice(-1);
-        const lastCharacterPlus = String(parseInt(lastCharacter) + num);
-        const finalDate = date.substring(0, 9) + lastCharacterPlus;
+  const dateComp = (dateFilter) => {
+    const currenDate = new Date();
 
-        return finalDate
+    if (dateFilter == "today") {
+      const filteredMeals = meals.filter((meal) => {
+        return meal.date === datePrep(currenDate);
+      });
+      return filteredMeals;
+    } else if (dateFilter == "1") {
+      currenDate.setDate(currenDate.getDate() + 1);
+      const filteredMeals = meals.filter((meal) => {
+        return meal.date === datePrep(currenDate);
+      });
+      return filteredMeals;
+    } else if (dateFilter === "2") {
+      currenDate.setDate(currenDate.getDate() + 2);
+      const filteredMeals = meals.filter((meal) => {
+        return meal.date === datePrep(currenDate);
+      });
+      return filteredMeals;
+    } else if (dateFilter === "3") {
+      currenDate.setDate(currenDate.getDate() + 3);
+      const filteredMeals = meals.filter((meal) => {
+        return meal.date === datePrep(currenDate);
+      });
+      return filteredMeals;
+    } else if (dateFilter === "4") {
+      currenDate.setDate(currenDate.getDate() + 4);
+      const filteredMeals = meals.filter((meal) => {
+        return meal.date === datePrep(currenDate);
+      });
+      return filteredMeals;
+    } else if (dateFilter === "5") {
+      currenDate.setDate(currenDate.getDate() + 5);
+      const filteredMeals = meals.filter((meal) => {
+        return meal.date === datePrep(currenDate);
+      });
+      return filteredMeals;
+    } else if (dateFilter === "6") {
+      currenDate.setDate(currenDate.getDate() + 6);
+      const filteredMeals = meals.filter((meal) => {
+        return meal.date === datePrep(currenDate);
+      });
+      return filteredMeals;
+    } else if (dateFilter === "7") {
+      currenDate.setDate(currenDate.getDate() + 7);
+      const filteredMeals = meals.filter((meal) => {
+        return meal.date === datePrep(currenDate);
+      });
+      return filteredMeals;
     }
+  };
 
-    const dateComp = (dateFilter) => {
+  ////////////////////// Format and Return Requested Info
 
-        const currentDate = String(new Date());
-        const formatedCurrentDate = dayjs(currentDate).format("YYYY MM DD").replaceAll(" ", "-");
+  const displayListElements = (list) => {
+    const listElement = list.split(",");
+    return (
+      <ul className="list-disc">
+        {listElement.map((element, index) => (
+          <li key={index}>{element.trim()}</li>
+        ))}
+      </ul>
+    );
+  };
 
-        if (dateFilter == "today") {
-            const filteredMeals = meals.filter(meal => { return datePrep(meal.date, 0) === formatedCurrentDate });
-            return filteredMeals
-        } else if (dateFilter == "1") {
-            const filteredMeals = meals.filter(meal => { return datePrep(meal.date, 0) === datePrep(formatedCurrentDate, 1); });
-            return filteredMeals
-        } else if (dateFilter === "2") {
-            const filteredMeals = meals.filter(meal => { return datePrep(meal.date, 0) == datePrep(formatedCurrentDate, 2); });
-            return filteredMeals
-        }
-    }
+  const MealName = (dateFilter) => {
+    return (
+      <div className="list-disc">
+        {dateComp(dateFilter).map((meal) => (
+          <div key={meal.id}>{meal.name}</div>
+        ))}
+      </div>
+    );
+  };
 
-    ////////////////////// Format and Return Requested Info
+  const MealIngredients = (dateFilter) => {
+    return (
+      <div>
+        {dateComp(dateFilter).map((meal) => (
+          <div key={meal.id}>{displayListElements(meal.ingredients)}</div>
+        ))}
+      </div>
+    );
+  };
 
-    const displayListElements = (list) => {
-        const listElement = list.split(",");
-        return (<ul className="list-disc">
-            {listElement.map((element, index) => (
-                <li key={index}>{element.trim()}</li>
-            ))}
-        </ul>);
-    }
+  const MealRecipe = (dateFilter) => {
+    return (
+      <div>
+        {dateComp(dateFilter).map((meal) => (
+          <div key={meal.id}>{displayListElements(meal.recipe)}</div>
+        ))}
+      </div>
+    );
+  };
 
-
-    const MealName = (dateFilter) => {
-
-        return (
-            <div className='list-disc'>
-                {dateComp(dateFilter).map((meal) => (
-                    <div key={meal.id}>{meal.name}</div>
-                ))
-                }
-            </div >
-        )
-    }
-
-
-
-    const MealIngredients = (dateFilter) => {
-        return (
-            <div>
-                {dateComp(dateFilter).map((meal) => (
-                    <div key={meal.id}>{displayListElements(meal.ingredients)}</div>
-                ))}
-            </div>
-        );
-    }
-
-    const MealRecipe = (dateFilter) => {
-
-        return (
-            <div>
-                {dateComp(dateFilter).map((meal) => (
-                    <div key={meal.id}>{displayListElements(meal.recipe)}</div>
-                ))}
-            </div>
-        );
-    }
-
-    return { MealIngredients, MealRecipe, MealName };
-
+  return { MealIngredients, MealRecipe, MealName };
 };
 
-
-export default MealTransfer
-
-
+export default MealTransfer;

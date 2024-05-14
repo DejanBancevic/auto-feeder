66545a1ad4dc9453a3f80ac5a024b36c4e724099
DB <alies22222@gmail.com>
2024-05-14 12:50:12 +0200

    user logic deploy v3

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,23 +2,10 @@ import NextAuth, {AuthOptions} from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from '../../../../lib/prisma';
+import { authOptions } from "../../../../lib/authOp";
 
-const authOptions:AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    debug: process.env.NODE_ENV === 'development',
-    session: {
-        strategy: "jwt"
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-    ],
-}
 
 export const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST, authOptions };
+export { handler as GET, handler as POST };
 
---
+++ lib/authOp.ts
@@ -0,0 +1,19 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from './prisma';
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ],
+}

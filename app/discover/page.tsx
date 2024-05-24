603d7df54aeddb4f8f09c9e78c3a1ac19ac163c8
DB <alies22222@gmail.com>
2024-05-24 07:46:07 +0200

    prep to production and main merge

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -6,7 +6,7 @@ const Discover = async () => {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-18">
-           DA
+           <h1 className="flex justify-center">In the works...</h1>
          
         </main>
     );

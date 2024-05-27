0cd15dfd56c06450f7120ffb5a0bd2d14ef6561d
DB <alies22222@gmail.com>
2024-05-27 09:38:25 +0200

    google oauth added

--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -6,9 +6,14 @@ import Image from 'next/image';
 
 const Login = () => {
 
-    const handleSignIn = () => {
+    const handleSignInGithub = () => {
         const callbackUrl = `${window.location.origin}/home`;
         return signIn('github', { callbackUrl });
+    };
+
+    const handleSignInGoogle = () => {
+        const callbackUrl = `${window.location.origin}/home`;
+        return signIn('google', { callbackUrl });
     };
 
     return (
@@ -25,9 +30,15 @@ const Login = () => {
             <div className="relative flex justify-center my-16 md:my-18 w-full h-64 md:h-96">
                 <button
                     className="btn-land text-xl px-4 md:px-6 py-3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
-                    onClick={handleSignIn}
+                    onClick={handleSignInGithub}
                 >
-                    Sign in
+                    Sign in with Github
+                </button>
+                <button
+                    className="btn-land text-xl px-4 md:px-6 py-3 absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+                    onClick={handleSignInGoogle}
+                >
+                    Sign in with Google
                 </button>
                 <Image
                     src={backgroundImage}
